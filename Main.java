25b66b793c6ceacc5f8c8ef854567c240deeb7d5
Tyler <your-email@example.com>
2026-09-23 16:02:09 +1200

    Updated Main.java with second output line

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Task 9 look at me im a silly lil program");
+        System.out.println("This is my second silly little commit for Task 9");
     }
 }
